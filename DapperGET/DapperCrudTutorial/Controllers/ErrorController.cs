2ab9b4088add1c5c259784fb5dcd72c837d9acd3
vleboux <144213384+vleboux@users.noreply.github.com>
2023-11-14 09:21:39 +0100

    Add files via upload

---
+++ DapperGET/DapperCrudTutorial/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperGET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ErrorController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Error>>> GetAllEvents()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var errors = await connection.QueryAsync<Error>("select * from processor.events");
+            return Ok(errors);
+        }
+
+        
+    }
+}
---
+++ DapperGET/DapperCrudTutorial/Error.cs
@@ -0,0 +1,16 @@
+﻿namespace DapperGET
+{
+    public class Error
+    {
+        public DateTime ThrownAt { get; set; }
+        public int LogLevel { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public Dictionary<string, string>? Parameters { get; set; } // Assuming it's a key-value dictionary
+        public string ExceptionType { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public List<string>? Claims { get; set; }
+
+    }
+}
